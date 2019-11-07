8dc7ae625f3fa982f228a2d4e934387fe1291d85
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2019-11-06 23:46:53 -0500

    first commit

---
+++ src/main/java/com/tecsup/petclinic/service/OwnerService.java
@@ -0,0 +1,56 @@
+package com.tecsup.petclinic.service;
+
+import java.util.List;
+
+import com.tecsup.petclinic.domain.Owner;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
+
+/**
+ * 
+ * @author jgomezm
+ *
+ */
+public interface OwnerService {
+
+	/**
+	 * 
+	 * @param pet
+	 * @return
+	 */
+	Owner create(Owner pet);
+
+	/**
+	 * 
+	 * @param pet
+	 * @return
+	 */
+	Owner update(Owner pet);
+
+	/**
+	 * 
+	 * @param id
+	 * @throws OwnerNotFoundException
+	 */
+	void delete(Long id) throws OwnerNotFoundException;
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	Owner findById(long id) throws OwnerNotFoundException;
+
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	List<Owner> findByName(String name);
+
+	/**
+	 * 
+	 * @return
+	 */
+	Iterable<Owner> findAll();
+
+}
